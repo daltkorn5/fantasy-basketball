00f7fd4ff555798e15c13de811a5dbddf2a57cd5
Danny Altkorn <daltkorn5@gmail.com>
2021-11-14 13:21:05 -0600

    added some stuff to help with setting up the database

---
+++ sql/initalize_db.sql
@@ -0,0 +1,8 @@
+/* script to create the 7 tables in the correct order */
+\i sql/create_table_teams.sql
+\i sql/create_table_nba_teams.sql
+\i sql/create_table_players.sql
+\i sql/create_table_match_ups.sql
+\i sql/create_table_nba_schedule.sql
+\i sql/create_table_rosters.sql
+\i sql/create_table_game_log.sql
