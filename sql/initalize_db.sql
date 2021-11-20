c67f1600c9aa14cc2c4b6453cdf272b72015a210
Danny Altkorn <daltkorn5@gmail.com>
2021-11-20 15:12:16 -0600

    created two new view definition files

--- sql/initalize_db.sql
+++ sql/initalize_db.sql
@@ -1,8 +1,12 @@
-/* script to create the 7 tables in the correct order */
-\i sql/create_table_teams.sql
-\i sql/create_table_nba_teams.sql
-\i sql/create_table_players.sql
-\i sql/create_table_match_ups.sql
-\i sql/create_table_nba_schedule.sql
-\i sql/create_table_rosters.sql
-\i sql/create_table_game_log.sql
+/* Create the 7 tables in the correct order */
+\i sql/table_definitions/create_table_teams.sql
+\i sql/table_definitions/create_table_nba_teams.sql
+\i sql/table_definitions/create_table_players.sql
+\i sql/table_definitions/create_table_match_ups.sql
+\i sql/table_definitions/create_table_nba_schedule.sql
+\i sql/table_definitions/create_table_rosters.sql
+\i sql/table_definitions/create_table_game_log.sql
+
+/* Then create the views */
+\i sql/view_definitions/create_view_schedule_per_team.sql
+\i sql/view_definitions/create_view_game_count_per_team_per_week.sql
